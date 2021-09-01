464491bdbb588baf0d31684a2770c07609a98042
backcoc <yryousafraza@gmail.com>
2021-09-01 21:52:46 +0500

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '50e35f7b2655751d12b0f786f9794224'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'd0f9c94c-784e-4c8d-99ac-674ba87f3041'
+};
