0b73498a5bfe205952aa6b3c5bad33cfcddf07a3
backcoc <yryousafraza@gmail.com>
2021-09-01 22:10:42 +0500

    second

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 5000),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '50e35f7b2655751d12b0f786f9794224'),
